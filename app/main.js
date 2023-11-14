6d6e8aa4b8038f196b8365fc924d44a16853bf71
Noah <100672306+eacxyz@users.noreply.github.com>
2023-11-14 16:20:21 -0500

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -5,6 +5,12 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    const httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+    socket.write(httpResponse);
+    socket.end();
+  });
+
   socket.on("close", () => {
     socket.end();
     server.close();
